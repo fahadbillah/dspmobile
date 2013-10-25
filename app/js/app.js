7b1a9c746afb73b492f4080bc7ffafd85ff6f2c3
fahadbillah <fahadbillah@yahoo.com>
2013-10-25 07:29:31 +0600

    google map integrated for the first time

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,16 @@
 var DSApp = angular.module('DSApp', [
   'ngRoute',
   'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'DSController'
+  'DSServices',
+  'DSApp.directives',
+  'DSController',
+  'AngularGM'
 ])
 DSApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
   $routeProvider.when('/shops', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
+
+
+//var Map = angular.module('DSApp', ['AngularGM']);
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,7 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+angular.module('DSApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
